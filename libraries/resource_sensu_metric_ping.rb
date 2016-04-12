ed8ee655ef18e478b2461dd40cff427c93edb8b3
Christian Graf <email@christiangraf.de>
2016-04-12 16:25:35 +0200

    Add ping metric

---
+++ libraries/resource_sensu_metric_ping.rb
@@ -0,0 +1,20 @@
+require "chef/resource"
+
+class Chef
+  class Resource
+    class SensuPluginMetricPing < Chef::Resource::SensuPluginMetricGeneric
+
+      def initialize(name, run_context=nil)
+        super
+        @cmd_name = "metrics-ping.rb"
+        @local_files << {name: @cmd_name, source: "metrics/#{@cmd_name}"}
+      end
+
+      set_attrib :scheme,    :cmd_flag => '-s', :kind_of => String, :required => true
+      set_attrib :host,      :cmd_flag => '-h', :kind_of => String
+      set_attrib :count,     :cmd_flag => '-c', :kind_of => Integer
+      set_attrib :timeout,   :cmd_flag => '-t', :kind_of => Integer
+
+    end
+  end
+end
