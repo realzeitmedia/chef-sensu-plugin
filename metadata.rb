890324131a94c70360ec087d41d45f3d4ff9676e
Christian Graf <email@christiangraf.de>
2016-04-12 16:25:50 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'christian.graf@realzeitmedia.com'
 license          'Apache 2.0'
 description      'Installs/Configures Sensu checks, handlers, etc. using community plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.14'
+version          '0.1.15'
 
 depends 'sensu'
